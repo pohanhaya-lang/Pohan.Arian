a17c2f2522d866d4a508c43adfdffd27a5ad5d95
Pohan mohammadkhil <pohan.haya@gmail.com>
2026-03-14 21:53:37 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -10,11 +10,24 @@ correct: 1
 
 {
 question: "تفسیر د اسلوب له مخې په څو ډوله دی؟",
-answers: ["دوه","درې","څلور","شپږ"],
+answers: ["دوه ډوله","درې ډوله","څلور ډوله","شپږ ډوله"],
 correct: 0
+},
+
+{
+question: "د تفسیر مهم کتابونه کوم دي؟",
+answers: ["طبري","بیضاوي","کشاف","ټول سم دي"],
+correct: 3
+},
+
+{
+question: "ترجمان القران د کوم صحابي لقب و؟",
+answers: ["عبدالله بن عمر","عبدالله بن عباس","عمر","حمزه"],
+correct: 1
 }
 
 ],
+
 
 math: [
 
@@ -22,9 +35,22 @@ math: [
 question: "2 + 2 = ؟",
 answers: ["3","4","5","6"],
 correct: 1
+},
+
+{
+question: "5 × 5 = ؟",
+answers: ["10","20","25","30"],
+correct: 2
+},
+
+{
+question: "10 ÷ 2 = ؟",
+answers: ["2","3","4","5"],
+correct: 3
 }
 
 ],
+
 
 biology: [
 
@@ -32,6 +58,12 @@ biology: [
 question: "Human heart has how many chambers?",
 answers: ["2","3","4","5"],
 correct: 2
+},
+
+{
+question: "Photosynthesis occurs in which organ?",
+answers: ["Leaf","Root","Stem","Flower"],
+correct: 0
 }
 
 ]
