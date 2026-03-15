fe8a7b1281e3a10d98166b009d9448b5f2a94b7a
Pohan mohammadkhil <pohan.haya@gmail.com>
2026-03-14 20:41:59 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,63 @@
+let currentQuestion = 0;
+let score = 0;
+let selectedQuestions = [];
 
+function startQuiz(){
+
+let subject = document.getElementById("subject").value;
+
+selectedQuestions = questions[subject];
+
+currentQuestion = 0;
+score = 0;
+
+showQuestion();
+
+}
+
+function showQuestion(){
+
+let q = selectedQuestions[currentQuestion];
+
+let html = "<h2>"+q.question+"</h2>";
+
+for(let i=0;i<q.answers.length;i++){
+
+html += "<button onclick='checkAnswer("+i+")'>"
++ q.answers[i] +
+"</button><br><br>";
+
+}
+
+document.getElementById("quiz").innerHTML = html;
+
+}
+
+function checkAnswer(i){
+
+if(i === selectedQuestions[currentQuestion].correct){
+
+score++;
+
+}
+
+nextQuestion();
+
+}
+
+function nextQuestion(){
+
+currentQuestion++;
+
+if(currentQuestion < selectedQuestions.length){
+
+showQuestion();
+
+}else{
+
+document.getElementById("quiz").innerHTML = "Quiz Finished";
+document.getElementById("result").innerText = "Score: " + score;
+
+}
+
+}
