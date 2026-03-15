4a9513d5d28822b5d3a4d9b004637c4e5f05c9ac
Pohan mohammadkhil <pohan.haya@gmail.com>
2026-03-14 20:41:19 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,39 @@
+const questions = {
 
+tafsir: [
+
+{
+question: "په کوم تفسیر عمل روا نه دی؟",
+answers: ["بالرای المحمود","بالرای المذموم","بالماثور","نقلي"],
+correct: 1
+},
+
+{
+question: "تفسیر د اسلوب له مخې په څو ډوله دی؟",
+answers: ["دوه","درې","څلور","شپږ"],
+correct: 0
+}
+
+],
+
+math: [
+
+{
+question: "2 + 2 = ؟",
+answers: ["3","4","5","6"],
+correct: 1
+}
+
+],
+
+biology: [
+
+{
+question: "Human heart has how many chambers?",
+answers: ["2","3","4","5"],
+correct: 2
+}
+
+]
+
+};
